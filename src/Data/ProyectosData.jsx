bcbb82202d7dc23668597a0f286657b8e9fe9227
mago_d_Mercurio <davila.feria@gmail.com>
2023-06-22 15:15:46 -0500

    add proyectos

--- src/Data/ProyectosData.jsx
+++ src/Data/ProyectosData.jsx
@@ -1,39 +1,46 @@
 
-
-import proyectoimg1 from "../assets/images/React_logo_wordmark.png";
-import proyectoimg2 from "../assets/images/React_logo_wordmark.png";
-import proyectoimg3 from "../assets/images/React_logo_wordmark.png";
-
+import proyectImg1 from "../assets/images/devflix.png";
+import proyectImg2 from "../assets/images/org.png";
+import proyectImg3 from "../assets/images/barberiaimg.png";
+import proyectImg4 from "../assets/images/smartbank.png";
+import proyectImg5 from "../assets/images/yardsale.png";
+import proyectImg6 from "../assets/images/todoimg1.png";
 
 export const proyectosData = [
   {
-    proyectName: "proyecto1",
-    description: "description1",
-    image: proyectoimg1,
+    proyectName: "Devflix",
+    description: "Plataforma de videos que permite agregar y eliminar videos de cualquier canal de youtube",
+    image: proyectImg1,
+    urlProyect: "https://devflix2023.netlify.app/",
   },
   {
-    proyectName: "proyecto2",
-    description: "description2",
-    image: proyectoimg2,
+    proyectName: "Organizacion",
+    description: "Personas y Equipos organizados en un solo lugar",
+    image: proyectImg2,
+    urlProyect: "https://organizacion-software.vercel.app/",
   },
   {
-    proyectName: "proyecto3",
-    description: "description3",
-    image: proyectoimg3,
+    proyectName: "Barberia Vintage",
+    description: "Landing Page de una barberia",
+    image: proyectImg3,
+    urlProyect: "https://magomercurio.github.io/web-barberia/",
   },
   {
-    proyectName: "proyecto4",
-    description: "description4",
-    image: proyectoimg3,
+    proyectName: "Smart Bank",
+    description: "Front de gastos realizados dentro de aplicacion de un banco",
+    image: proyectImg4,
+    urlProyect: "https://smart-bank2023.netlify.app/",
   },
   {
-    proyectName: "proyecto5",
-    description: "description5",
-    image: proyectoimg3,
+    proyectName: "Yard Sale",
+    description: "Front de una tienda en linea",
+    image: proyectImg5,
+    urlProyect: "https://yardsale-only-front.netlify.app/",
   },
   {
-    proyectName: "proyecto6",
-    description: "description6",
-    image: proyectoimg3,
+    proyectName: "To Do List",
+    description: "Lista de tareas que se pueden completar",
+    image: proyectImg6,
+    urlProyect: "https://magomercurio.github.io/manipulando-dom-ToDo/",
   },
 ];
