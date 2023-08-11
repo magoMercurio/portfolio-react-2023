80abec26126378046cc950f15289b4db675999f4
mago_d_Mercurio <davila.feria@gmail.com>
2023-08-11 11:17:17 -0500

    add new proyect

--- src/Data/ProyectosData.jsx
+++ src/Data/ProyectosData.jsx
@@ -1,50 +1,58 @@
 
-import proyectImg1 from "../assets/images/mtp-landp.webp";
-import proyectImg2 from "../assets/images/rpsicologo.webp";
-import proyectImg3 from "../assets/images/devflix.webp";
-import proyectImg4 from "../assets/images/org.webp";
-import proyectImg5 from "../assets/images/barberiaimg.webp";
-import proyectImg6 from "../assets/images/smartbank.webp";
+import proyectImg1 from "../assets/images/rpsicologv2.webp";
+import proyectImg2 from "../assets/images/mtp-landp.webp";
+import proyectImg3 from "../assets/images/rpsicologo.webp";
+import proyectImg4 from "../assets/images/devflix.webp";
+import proyectImg5 from "../assets/images/org.webp";
+import proyectImg6 from "../assets/images/barberiaimg.webp";
+
+/* import proyectImg6 from "../assets/images/smartbank.webp"; */
 /* import proyectImg6 from "../assets/images/yardsale.webp"; */
 /* import proyectImg6 from "../assets/images/todoimg1.webp"; */
 
 export const proyectosData = [
   {
+    proyectName: "Raul Davila Psicologo V2",
+    description: "Landing-Page que cuenta con un Blog creada para un psicologo",
+    image: proyectImg1,
+    urlProyect: "https://rauldavilapsicologo.com",
+  },
+  {
     proyectName: "Maribel Tristan Psicologa",
     description: "Landing-Page creada para una psicologa",
-    image: proyectImg1,
+    image: proyectImg2,
     urlProyect: "https://terapiaconmaribel.com/",
   },
   {
-    proyectName: "Raul Davila Psicologo",
+    proyectName: "Raul Davila Psicologo V1",
     description: "Landing-Page creada para un psicologo",
-    image: proyectImg2,
-    urlProyect: "https://rauldavilapsicologo.com",
+    image: proyectImg3,
+    urlProyect: "https://raulpsicologo.netlify.app/",
   },
   {
     proyectName: "Devflix",
     description: "Plataforma de videos que permite agregar y eliminar videos de cualquier canal de youtube",
-    image: proyectImg3,
+    image: proyectImg4,
     urlProyect: "https://devflix2023.netlify.app/",
   },
   {
     proyectName: "Organizacion",
     description: "Personas y Equipos organizados en un solo lugar",
-    image: proyectImg4,
+    image: proyectImg5,
     urlProyect: "https://organizacion-software.vercel.app/",
   },
   {
     proyectName: "Barberia Vintage",
     description: "Landing Page de una barberia",
-    image: proyectImg5,
+    image: proyectImg6,
     urlProyect: "https://magomercurio.github.io/web-barberia/",
   },
-  {
+ /*  {
     proyectName: "Smart Bank",
     description: "Front de gastos realizados dentro de aplicacion de un banco",
     image: proyectImg6,
     urlProyect: "https://smart-bank2023.netlify.app/",
-  },
+  }, */
   /* {
     proyectName: "Yard Sale",
     description: "Front de una tienda en linea",
